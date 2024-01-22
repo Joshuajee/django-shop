696a4e510e22f125b1cb6b76435f4a8d6638b398
Taiwo Isaac <itaiwo318@gmail.com>
2024-01-22 10:04:41 +0100

    Initialised cart and authentication

---
+++ cart/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import CartItem
+from shop.models import Product
+
+
+def add_to_cart(request, product_id):
+    if request.method == 'POST':
+        product = Product.objects.get(pk=product_id)
+        user = request.user
+        quantity = int(request.POST.get('quantity', 1))
+        cart_item, created = CartItem.objects.get_or_create(user=user, product=product)
+        if not created:
+            cart_item.quantity += quantity
+            cart_item.save()
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    return render(request, 'cart/cart_detail.html', {'cart_items': cart_items,'total_price': total_price})
+
+
+        
+
+
